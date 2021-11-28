df4ade56240f6a93bc5960b7a7ab1ab76a476954
Pasan Narangoda <narangodapasan@gmail.com>
2021-11-28 15:54:07 +0530

    Orders view for admin not finished, encountered some issues

---
+++ frontend/src/mainPages/orders/OrdersAdmin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function OrderHistory() {
+    
+
+    return (
+        <div>
+            Orders viewing section for admin
+        </div>
+    )
+}
+// abandoned because of navigation problems
+
