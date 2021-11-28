6f743f8e9283f458f45e2f8262fcea51c6b174c1
Pasan Narangoda <narangodapasan@gmail.com>
2021-11-28 15:55:07 +0530

    Orders view for customer, not finished, met some issues of viewing the page

---
+++ frontend/src/mainPages/orders/OrdersCustomer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function OrdersCustomer() {
+
+    return (
+       <div>
+            Order view section for customer
+       </div>
+    )
+}
+
+// Problem encountered when navigating so abandoned
